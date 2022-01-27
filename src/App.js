2c79d85b8c53e4f0ada2c289cd9160caa5dfa10b
Ingrid_Ramos <ingridrcs@gmail.com>
2022-01-26 23:44:31 -0300

    requisitos 1 ao 5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
+import PlanetCard from './components/PlanetCard';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+        <Title />
+        <PlanetCard />
+      </>
+    );
   }
 }
 
